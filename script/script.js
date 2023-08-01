f07aa4b93a43063aafe03407fdeb8334f50431f3
Rafael Vogas Gama <rafaelvogasgama@id.uff.br>
2023-08-01 14:15:13 -0300

    Improving accordion list script

--- script/script.js
+++ script/script.js
@@ -2,14 +2,16 @@ const activeClass = "active";
 
 const headings = document.querySelectorAll("h2");
 
-headings[0].classList.add(activeClass);
-headings[0].nextElementSibling.classList.add(activeClass);
+if (headings.length) {
+  headings[0].classList.add(activeClass);
+  headings[0].nextElementSibling.classList.add(activeClass);
 
-function toggleExpandCollapse() {
-  this.classList.toggle(activeClass);
-  this.nextElementSibling.classList.toggle(activeClass);
+  function toggleExpandCollapse() {
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
+  }
+
+  headings.forEach((item) => {
+    item.addEventListener("click", toggleExpandCollapse);
+  });
 }
-
-headings.forEach((item) => {
-  item.addEventListener("click", toggleExpandCollapse);
-});
